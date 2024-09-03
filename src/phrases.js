bc6ef85fbad21495644f0d7a4c6da63ac327ff07
ihschmidt16 <168250431+ihschmidt16@users.noreply.github.com>
2024-09-02 23:51:41 -0400

    Add files via upload

---
+++ src/phrases.js
@@ -0,0 +1,37 @@
+
+
+
+
+const ALL_PHRASES = {
+
+'20240729': "FUZZY -PEACH- -STATE- OF THE UNION -ADDRESS- A -PROBLEM-ATIC PEOPLE",
+// -------------------------
+'20240901': "FI-RE-START",
+// -------------------------
+'20240902': "BITE THE -BULLET-PROOF -PLAN- OF -ACTION-S SPEAK LOUDER THAN -WORDS- OF -WISDOM- -TEETH-ING -PUPPY- -LOVE- ONE -ANOTHER- ONE BITES THE -DUST-ING -OFF- THE BEATEN PATH",
+'20240902_0': "Accept impending hardship isoaj iodsf oi soidfj oisd fiojoa iosjdfoi osdif sdifo iojd",
+'20240902_1': "Foolproof option",
+'20240902_2': "To accept impending hardship",
+'20240902_3': "To accept impending hardship",
+'20240902_4': "To accept impending hardship",
+'20240902_5': "To accept impending hardship",
+'20240902_6': "To accept impending hardship",
+'20240902_7': "To accept impending hardship",
+'20240902_8': "To accept impending hardship",
+'20240902_9': "To accept impending hardship",
+'20240902_10': "To accept impending hardship",
+// -------------------------
+'20240801': "EXTREMELY FAST AND ROGUE -BULLET-PROOF -GLASS- HALF -EMPTY- ON THE -INSIDE- -VOICE-S OF -TRUTH- BE TOLD",
+// -------------------------
+'20240802': "ONE MUST BE QUICK THROUGH THE AIR LIKE A SPEEDING -BULLET--PROOF--READ-Y FOR ANYTHING",
+// -------------------------
+'20240803': "BULLET-PROOF--READ- A CLASSMATES ESSAY FOR A GRADE IN A VERY DIFFICULT CLASS IN -COLLEGE- MIDTERMS",
+// -------------------------
+'20240820': "I LIKE -MILK BONES- ARE -GOOD- -GRIEF-ING A HU-MAN--SLAUGHTER-HOUSE",
+// -------------------------
+'20240826': "GO THE EXTRA -MILE- -HIGH- -FIVE- OCLOCK -SHADOW- OF A -DOUBT--LESS- IS -MORE- OR LESS"
+
+};
+
+
+export { ALL_PHRASES };
