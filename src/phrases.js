2c4fe13a547851705bfdada4598aae7a79376f5d
ihschmidt16 <168250431+ihschmidt16@users.noreply.github.com>
2024-09-17 21:11:10 -0400

    Add files via upload

---
+++ src/phrases.js
@@ -0,0 +1,44 @@
+
+
+
+
+const ALL_PHRASES = {
+    // -------------------------
+    '20221010': "GO THE EXTRA -MILE- -HIGH- -FIVE- OCLOCK -SHADOW- OF A -DOUBT--LESS- IS -MORE- OR LESS",
+    '20221010_0': 'Another way to say "Go above and beyond"',
+    '20221010_1': 'A nickname for Denver -(City)',
+    '20221010_2': 'A gesture for a job well done',
+    '20221010_3': 'Beard scruff',
+    '20221010_4': '"Her guilt was evident beyond the _____."',
+    '20221010_5': 'Beyond confident',
+    '20221010_6': '"Don\'t overdo it ,_____."',
+    '20221010_7': 'Approximately',
+    // -------------------------
+    '20221011': "BITE THE -BULLET-PROOF -PLAN- OF -ACTION-S SPEAK LOUDER THAN -WORDS- OF WISDOM",
+    '20221011_0': 'Accept impending hardship',
+    '20221011_1': '"Don\'t worry, I have a _____"',
+    '20221011_2': '"We must develop a _____ to solve the problem"',
+    '20221011_3': '"Prove it to me, because _____"',
+    '20221011_4': 'What eldery people give to younger people',
+    // -------------------------
+    '20221012': 'ELEPHANT IN THE -ROOM- THE -READ- LIGHT-LY-RICAL GENIUS',
+    '20221012_0': '"To get started, let\'s address the _____".',
+    '20221012_1': '"Stop killing the vibe, _____!" (backwards word order)',
+    '20221012_2': '"Don\'t mess with me! You better T_____!"',
+    '20221012_3': 'Some say Eminem is this',
+    // -------------------------
+    '20221013': 'OUT OF THE -BLUE- COLLAR -WORKER- -BEES- THE AND -BIRDS- OF A -FEATHER- IN YOUR -CAP-TURE THE -MOMENT- OF -SILENCE- OF THE LAMBS',
+    '20221013_0': 'Unforseen',
+    '20221013_1': 'Someone who doesn\'t work in an office',
+    '20221013_2': '"They are _____ for the company."',
+    '20221013_3': 'An uncomfortable talk a dad has with his son (backwards word order)',
+    '20221013_4': '"_____ stick together."',
+    '20221013_5': 'An amazing accoplishment',
+    '20221013_6': 'Take it all in',
+    '20221013_7': 'A way to honor someone\'s passing',
+    '20221013_8': 'Book with Hannibal Lecter',
+    // -------------------------
+    };
+    
+    
+    export { ALL_PHRASES };
