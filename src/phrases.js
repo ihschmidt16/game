bcd5a56bef44a164b67f6041db0aa93e46f1517f
ihschmidt16 <168250431+ihschmidt16@users.noreply.github.com>
2025-02-05 16:02:13 -0500

    Add files via upload

---
+++ src/phrases.js
@@ -0,0 +1,52 @@
+
+// NO COMBINED WORD LENGTHS GREATER THAN 12 LETTERS
+
+
+const ALL_PHRASES = {
+
+    '20240911': "FI-RE-START",
+    // -------------------------
+    '20250201': "GO THE EXTRA -MILE- -HIGH- -FIVE- OCLOCK -SHADOW- OF A -DOUBT--LESS- IS -MORE- OR LESS",
+    '20250201_0': 'Another way to say "Go above and beyond"',
+    '20250201_1': 'A nickname for Denver -(City)',
+    '20250201_2': 'A gesture for a job well done',
+    '20250201_3': 'Beard scruff',
+    '20250201_4': '"Her guilt was evident beyond the _____."',
+    '20250201_5': 'Beyond confident',
+    '20250201_6': '"Don\'t overdo it ,_____."',
+    '20250201_7': 'Approximately',
+    // -------------------------
+    '20250202': "BITE THE -BULLET-PROOF -PLAN- OF -ACTION-S SPEAK LOUDER THAN -WORDS- OF WISDOM",
+    '20250202_0': 'Accept impending hardship',
+    '20250202_1': '"Don\'t worry, I have a _____"',
+    '20250202_2': '"We must develop a _____ to solve the problem"',
+    '20250202_3': '"Prove it to me, because _____"',
+    '20250202_4': 'What eldery people give to younger people',
+    // -------------------------
+    '20250203': 'ELEPHANT IN THE -ROOM- THE -READ- LIGHT-LY-RICAL GENIUS',
+    '20250203_0': '"To get started, let\'s address the _____".',
+    '20250203_1': '"Stop killing the vibe, _____!" (backwards word order)',
+    '20250203_2': '"Don\'t mess with me! You better T_____!"',
+    '20250203_3': 'Some say Eminem is this',
+    // -------------------------
+    '20250205': 'OUT OF THE -BLUE- COLLAR -WORKER- -BEES- THE AND -BIRDS- OF A -FEATHER- IN YOUR -CAP-TURE THE -MOMENT- OF -SILENCE- OF THE LAMBS',
+    '20250205_0': 'Unforseen',
+    '20250205_1': 'Someone who doesn\'t work in an office',
+    '20250205_2': '"They are _____ for the company."',
+    '20250205_3': 'An uncomfortable talk a dad has with his son (backwards word order)',
+    '20250205_4': '"_____ stick together."',
+    '20250205_5': 'An amazing accoplishment',
+    '20250205_6': 'Take it all in',
+    '20250205_7': 'A way to honor someone\'s passing',
+    '20250205_8': 'Book with Hannibal Lecter',
+    // -------------------------
+    
+    
+    
+    
+    
+    
+    };
+    
+    
+    export { ALL_PHRASES };
